71fa8b1c2677054627d022c4dc2a23ab6155872f
Jerome1580 <raoju1580@qq.com>
2017-12-06 23:41:02 +0800

    新增商品，评价，商家组件

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
   <div id="app">
     <HeaderView></HeaderView>
     <div class="tab">
-      I am tab
+      <div class="tab-item">
+        <router-link :to="{path:'/goods'}">商品</router-link>
     </div>
-    <div class="content">
-      I am content
+    <div class="tab-item">
+       <router-link :to="{path:'/ratings'}">评论</router-link>
     </div>
+    <div class="tab-item">
+      <router-link :to="{path:'/seller'}">商家</router-link>
+      </div>
+    </div>
+    <router-view></router-view>
+
   </div>
 </template>
 
 <script>
-  import HeaderView from '@/components/Header/Header.vue'
+  import HeaderView from '@/components/header/Header.vue'
   export default {
     components:{
       HeaderView
@@ -19,6 +26,14 @@
   }
 </script>
 
-<style>
-
+<style lang="stylus" type="text/stylus">
+  #app
+    .tab
+      display:flex
+      width:100%
+      height:40px
+      line-height:40px
+      .tab-item
+        flex:1
+        text-align:center
 </style>
---
+++ src/components/goods/Goods.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        i am goods
+    </div>
+</template>
+<script type="text/ecmascript-6">
+
+</script>
+<style scoped lang="stylus" type="text/stylus">
+
+</style>
