5779434d3a4b98cae940fb4baec1ad58f8e4f35d
raoju <raoju1580@qq.com>
2017-12-06 15:30:46 +0800

    初始化ele-vue2.0项目

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HeaderView></HeaderView>
+    <div class="tab">
+      I am tab
+    </div>
+    <div class="content">
+      I am content
+    </div>
+  </div>
+</template>
+
+<script>
+  import HeaderView from '@/components/Header/Header.vue'
+  export default {
+    components:{
+      HeaderView
+    }
+  }
+</script>
+
+<style>
+
+</style>
